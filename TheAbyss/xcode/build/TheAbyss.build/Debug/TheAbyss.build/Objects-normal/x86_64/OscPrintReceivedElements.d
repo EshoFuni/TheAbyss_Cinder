8e36f298fcd2b44bf87eab66c1a9806108891355
p1nh0 <p1nh0.c0d1ng@gmail.com>
2015-09-28 23:28:22 +0100

    Manual merge (part 1)

--- TheAbyss/xcode/build/TheAbyss.build/Debug/TheAbyss.build/Objects-normal/x86_64/OscPrintReceivedElements.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Applications/cinder_0.8.6_mac/blocks/OSC/src/osc/OscPrintReceivedElements.cpp \
-  /Applications/cinder_0.8.6_mac/blocks/OSC/src/osc/OscPrintReceivedElements.h \
-  /Applications/cinder_0.8.6_mac/blocks/OSC/src/osc/OscReceivedElements.h \
-  /Applications/cinder_0.8.6_mac/blocks/OSC/src/osc/OscTypes.h \
-  /Applications/cinder_0.8.6_mac/blocks/OSC/src/osc/OscException.h
--- TheAbyss/xcode/build/TheAbyss.build/Debug/TheAbyss.build/Objects-normal/x86_64/b2Rope.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Applications/cinder_0.8.6_mac/blocks/Box2D/src/Box2D/Rope/b2Rope.cpp \
-  ../../../../../../../Applications/cinder_0.8.6_mac/blocks/Box2D/src/Box2D/Rope/b2Rope.h \
-  ../../../../../../../Applications/cinder_0.8.6_mac/blocks/Box2D/src/Box2D/Common/b2Math.h \
-  ../../../../../../../Applications/cinder_0.8.6_mac/blocks/Box2D/src/Box2D/Common/b2Settings.h \
-  ../../../../../../../Applications/cinder_0.8.6_mac/blocks/Box2D/src/Box2D/Common/b2Draw.h
