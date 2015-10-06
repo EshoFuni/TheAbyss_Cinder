c3b5b3ce60437cd21ed12787401bf0e8ba6da096
p1nh0 <p1nh0.c0d1ng@gmail.com>
2015-10-06 16:15:50 +0100

    Added FadeIn() and FadeOut()

--- TheAbyss/src/TheAbyssApp.cpp
+++ TheAbyss/src/TheAbyssApp.cpp
@@ -17,6 +17,8 @@ class TheAbyssApp : public AppNative {
 	void update();
 	void draw();
     void keyDown(KeyEvent event);
+    void fadeIn();
+    void fadeOut();
     
     // DECLARATIONS
 //  int frameCount;
@@ -24,6 +26,15 @@ class TheAbyssApp : public AppNative {
     AbyssGUI mAbyssGUI; // helpful GUI (press 'H')
     CreatureManager manager; // class to manage all creatures
     BCIWave mBCIWave1, mBCIWave2; // waveform representation of BCI data
+
+    // FADE VARIABLES
+    int fadeTime = 10000;
+    float fadeInC = 1.f;
+    float fadeOutC = 0.f;
+    bool fade_in = false;
+    bool fade_inBegin = true;
+    bool fade_out = false;
+    Rectf r;
 };
 
 void TheAbyssApp::prepareSettings(Settings *settings){
@@ -39,6 +50,9 @@ void TheAbyssApp::setup()
     
     // WINDOW NAMING
     ci::app::getWindow()->setTitle("TheAbyss"); // added
+    
+    // FADEs
+    r = *new Rectf(0, 0, getWindowWidth(), getWindowHeight());
 
     // INSTATIATE AND INIT CAMERA
     mCam = *new Cam();
@@ -76,8 +90,7 @@ void TheAbyssApp::draw()
     
     // SET CAMERA
     mCam.setCam();
-    // DRAW ABYSSGUI
-    mAbyssGUI.draw();
+    
     // CREATURE DRAW
     manager.draw();
     
@@ -85,6 +98,12 @@ void TheAbyssApp::draw()
     mBCIWave1.draw();
     mBCIWave2.draw();
     
+    // DRAW FADES
+    fadeIn();
+    fadeOut();
+    
+    // DRAW ABYSSGUI
+    mAbyssGUI.draw();
 }
 
 // KEYBOARD SHORTCUTS
@@ -101,6 +120,54 @@ void TheAbyssApp::keyDown(KeyEvent event){
     }
     if (event.getChar() == 'w'){
         manager.addWorm();
+    }
+    if (event.getChar() == 'k'){
+        manager.compPlus();
+    }
+    if (event.getChar() == 'l'){
+        manager.compMinus();
+    }
+    if (event.getChar() == 'i'){
+        if(fade_in == false){ fade_in = true; fade_out = false; }
+    }
+    if (event.getChar() == 'o'){
+        if (fade_out == false){fade_out = true; }
+    }
+    if (event.getChar() == 'b') { // BEGIN
+        fade_inBegin = true;
+    }
+    
+}
+
+void TheAbyssApp::fadeIn(){
+    if (fade_inBegin == true && fade_in == false){
+        gl::color(0.f, 0.f, 0.f, 1.f);
+        gl::drawSolidRect(r);
+    }
+    if (fade_in == true){
+        if (fadeInC > 0.f){
+            fadeInC -= 1000.f / getFrameRate() / fadeTime;
+            gl::color(0.f, 0.f, 0.f, fadeInC);
+            gl::drawSolidRect(r);
+        }
+        if ( fadeInC <= 0.f ) {
+            fade_in = false;
+            fade_inBegin = false;
+            fadeInC = 1.f;
+        }
+    }
+}
+
+void TheAbyssApp::fadeOut(){
+    if (fade_out == true){
+        if (fadeOutC < 1.f){
+            fadeOutC += 1000.f / getFrameRate() / fadeTime;
+            gl::color(0.f, 0.f, 0.f, fadeOutC);
+        }
+        if ( fadeOutC >= 1.f ) {
+            fadeOutC = 1.f;
+        }
+        gl::drawSolidRect(r);
     }
 }
 
